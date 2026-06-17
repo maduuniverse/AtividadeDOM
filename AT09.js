023696e49ab6c366bce4ba901ffa0a657bda89bc
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-17 08:13:22 -0300

    Create AT09.js

---
+++ AT09.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 id="titulo">Título</h1>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
+
+<!--Script-->
+let h1 = document.getElementById("titulo");
+
+h1.style.color = "green";
+h1.style.backgroundColor = "yellow";
