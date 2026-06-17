bcf7e6701e9d943903e360bb4b2925a8d45d8399
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-17 08:07:01 -0300

    Create AT06.js

---
+++ AT06.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="banner" style="height:100px;background:white;">
+        Banner
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
+
+<!--Script-->
