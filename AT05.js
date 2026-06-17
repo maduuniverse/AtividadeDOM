2cb0e0f4e97912c777fb13ab34c7cec7602c63f3
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-17 08:03:54 -0300

    Create AT05.js

---
+++ AT05.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p id="texto" style="background:lightgray;">
+        Texto de exemplo
+    </p>
+
+</body>
+
+</html>
+
+<!--Script-->
+let p = document.getElementById("texto");
+
+p.style.backgroundColor = "black";
+p.style.color = "white";
