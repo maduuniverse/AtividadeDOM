4803449227bd787357bba0b926d790f20b5a6c34
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-17 07:54:35 -0300

    Create AT02.js

---
+++ AT02.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #retangulo {
+            width: 200px;
+            height: 100px;
+            background: blue;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="retangulo"></div>
+    <script src="script.js"></script>
+
+</body>
+
+</html>
+
+<!--Script-->
+document.getElementById("retangulo").style.backgroundColor = "red";
