25b6b71dc5607f7972181e3e8883af642bb49386
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-17 08:19:44 -0300

    Create AT12.js

---
+++ AT12.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #oval {
+            width: 200px;
+            height: 100px;
+            background: gray;
+            border-radius: 50%;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="oval"></div>
+    
+    <script src="script.js"></script>
+</body>
+
+</html>
+
+<!--Script-->
+document.getElementById("oval").style.backgroundColor="cyan";
