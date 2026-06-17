b31d485e151ed0f317aff53640703603b9e2ccaf
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-17 07:53:45 -0300

    Create AT01.js

---
+++ AT01.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #quadrado {
+            width: 100px;
+            height: 100px;
+            background: gray;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="quadrado"></div>
+    <script src="script.js"></script>
+
+</body>
+
+</html>
+
+<!--Script-->
+document.getElementById("quadrado").style.backgroundColor = "green";
